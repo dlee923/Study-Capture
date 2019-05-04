c70d83919986b33dba0f4103bf66151579f514b0
Daniel Lee <dlee923@gmail.com>
2019-05-03 20:46:17 -0400

    Set up option on MainViewController to segue to SelfieViewController

---
+++ Study Capture/SelfieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelfieViewController.swift
+//  Study Capture
+//
+//  Created by Daniel Lee on 5/3/19.
+//  Copyright © 2019 DLEE. All rights reserved.
+//
+
+import UIKit
+
+class SelfieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
