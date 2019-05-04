45b4d762bf4d11e65cc4c7b16ff2ba6124b40475
Daniel Lee <dlee923@gmail.com>
2019-05-03 20:32:37 -0400

    Creating initial file

--- Study Capture/AppDelegate.swift
+++ Study Capture/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let mainViewController = MainViewController()
+        let navController = UINavigationController(rootViewController: mainViewController)
+        
+        window?.rootViewController = navController
+        
         return true
     }
 
@@ -44,3 +53,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+/*
+ Project: App
+ Study Capture App
+ 
+ Background
+ Our clinical services team has come to us for help in developing an iPhone app for study capture. This app will allow a user to enter their subject id into a text box and capture a selfie image in portrait orientation with the front camera. The image will then need to have the subject’s id “burned in” to the lower right hand corner after having a core image filter applied, and then saved  to the camera roll.
+ 
+ Goals
+ The overall goal of this application is to allow the user to capture a selfie image to be later submitted for analysis.
+ 
+ Constraints
+ The subject id is alphanumeric.
+ The subject id written into the image needs to be in a white font on a black rectangular background.
+ The core image filter to be applied is a CIUnsharpMask with an input radius of 11 and an input intensity of 0.5.
+ The workflow of the app is: Enter subject id – capture image – review image – process image – display result image.
+ Display result image view needs to have a save button which will cause the displayed image to be saved to the camera roll.
+ 
+ For extra credit, display a custom spinner that you have written in the center of the screen while the image is being processed.
+ 
+ */
